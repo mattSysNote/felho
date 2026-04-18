a33fbdbb79796382f74e209b95c514f9598e9268
mattSysNote <127503529+mattSysNote@users.noreply.github.com>
2026-04-18 13:07:49 +0200

    CSP

--- photoupload/settings.py
+++ photoupload/settings.py
@@ -51,7 +51,6 @@ MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'whitenoise.middleware.WhiteNoiseMiddleware', 
     'django.contrib.sessions.middleware.SessionMiddleware',
-    'django.middleware.csp.ContentSecurityPolicyMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
